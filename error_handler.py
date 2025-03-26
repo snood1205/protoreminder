04962fbd51fea61ee70ea1ba643eed66e2b61785
Eli <9064062+snood1205@users.noreply.github.com>
2025-03-26 11:40:34 -0400

    Small bug fix :bug:

--- error_handler.py
+++ error_handler.py
@@ -11,7 +11,7 @@ def handle_no_run_at(did, parent_cid, parent_uri):
         did,
         ", unfortunately I was unable to parse your time. Please try again in a different format (e.g. '50 minutes' or 'January 1, 2040')",
     )
-    post_reply(post, parent_cid, parent_uri)
+    post_reply(post, str(parent_cid), parent_uri)
 
 
 def handle_run_at_in_past(did, parent_cid, parent_uri):
@@ -22,4 +22,4 @@ def handle_run_at_in_past(did, parent_cid, parent_uri):
         did,
         ", your reminder time appears to be in the past. I can only handle reminders for the future.",
     )
-    post_reply(post, parent_cid, parent_uri)
+    post_reply(post, str(parent_cid), parent_uri)
